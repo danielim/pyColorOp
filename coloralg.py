f01a2b8e7d3f50deb79a8e46aa540bf48e1e9ac1
Daniel Chen <yyyspam@gmail.com>
2014-07-22 17:20:08 -0500

    formatting

--- coloralg.py
+++ coloralg.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
-'''Color analysis as per CIEDE2000 Delta E formula, implemented with python-colormath by Gregory Taylor https://github.com/gtaylor/python-colormath'''
+'''Color analysis as per CIEDE2000 Delta E formula, implemented with
+python-colormath by Gregory Taylor 
+https://github.com/gtaylor/python-colormath'''
 
